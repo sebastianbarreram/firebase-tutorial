1561270f396a06489860ddf29356f844445ebb49
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-02 10:52:57 -0500

    Get data from Cloud Firestore of firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            {" "}
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
